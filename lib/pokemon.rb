a8a02f2520debf14c5c9d2dabdfd76a70bc2614a
Kal Megati <kalmegati@gmail.com>
2021-01-11 15:05:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,10 +11,24 @@ class Pokemon
   
   def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, type, db)
-      VALUES (?, ?, ?)
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
     SQL
-    DB
+    db.execute(sql, name, type)
+  end
+  
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = ?
+    SQL
+    pokerow = db.execute(sql, id)[0]
+    pokehash = {
+      id: pokerow[0],
+      name: pokerow[1],
+      type: pokerow[2],
+      db: db
+    }
+    Pokemon.new(pokehash)
   end
   
 end
